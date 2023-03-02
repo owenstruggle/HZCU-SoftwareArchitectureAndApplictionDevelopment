273a9485444f13ba35adadec2fb40efcbe101a44
OwemShu <shuhenxin@qq.com>
2023-03-02 23:31:50 +0800

    完成实验一

---
+++ Experiment01/src/main/java/cn/owemshu/experiment01/Experiment01Application.java
@@ -0,0 +1,13 @@
+package cn.owemshu.experiment01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Experiment01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Experiment01Application.class, args);
+	}
+
+}
---
+++ Experiment01/src/test/java/cn/owemshu/experiment01/Experiment01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.owemshu.experiment01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experiment01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
